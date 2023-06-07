724cd2d754a178a8ec8976066f20a4cbc02ca3f6
Idrissa <idrisabkar2018@gmail.com>
2023-06-07 14:15:21 +0300

    adding components file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '../App.css'
 
 function App() {
-  return
+  return <h1>Hello</h1>
 }
 
 export default App
---
+++ src/components/listTile.jsx
@@ -0,0 +1,4 @@
+function ListTile(props) {
+  const { image, name, age } = props
+  return <div className="listTile"></div>
+}
